58aed8334c2bf3bd97be9bd542f8ee3da47c4c7f
Darlan Cavalcante Moreira <darcamo@gmail.com>
2017-04-20 20:25:54 -0300

    Set bookmark-file

--- init.el
+++ init.el
@@ -374,6 +374,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (set-default 'bookmark-file "~/Dropbox/Dotfiles/.emacs.d/bookmarks")
   )
 
 (defun dotspacemacs/user-config ()
