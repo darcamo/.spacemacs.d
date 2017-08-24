8d1a801909c44489453246833e926ac591911647
Darlan Cavalcante Moreira <darcamo@gmail.com>
2017-08-24 16:58:34 -0300

    Define my own style with c-add-style

--- layers/darcamo-c++/config.el
+++ layers/darcamo-c++/config.el
@@ -10,20 +10,29 @@
 ;; Add this as a directory variable in the project
 ;; (set-default 'helm-make-build-dir "cmake-build-debug")
 
+
+;; If the indentation is wrong at some place, put the cursor there and use "C-c
+;; C-o" to find-out the offset and the name of the syntactical element under the
+;; cursor. Example: put the cursor in the "{" in a function definition and use
+;; "C-c C-o". Emacs will tell that the syntactical element is "defun-open". Now,
+;; see the help for the c-offsets-alist variable and look for defun-open to see
+;; what it is. Using this you can known how to adapt a style to your needs.
+;;
+;; Copied stroustrup style add add the "innamespace . 0"
+(c-add-style "darcamo"
+             '((c-doc-comment-style . javadoc)
+               (c-basic-offset . 4)
+               (c-comment-only-line-offset . 0)
+               (c-offsets-alist
+                (statement-block-intro . +)
+                (substatement-open . 0)
+                (substatement-label . 0)
+                (label . 0)
+                (statement-cont . +)
+                (innamespace . 0))))
+
 ;; Set default style to stroustrup
 (with-eval-after-load 'cc-vars
-  (push '(c-mode . "stroustrup") c-default-style)
-  (push '(c++-mode . "stroustrup") c-default-style)
+  (push '(c-mode . "darcamo") c-default-style)
+  (push '(c++-mode . "darcamo") c-default-style)
   )
-
-;; Disable indentation inside namespaces
-(add-hook 'c++-mode-hook
-          (progn (c-set-offset 'innamespace [0])))
-
-
-;; Note: You can define your own style using "c-add-style" function
-;; See this video (https://www.youtube.com/watch?v=OjbkCEkboA8&app=desktop) around 20minutes
-
-;; See the same video around 28 minutes for auto-completion
-;; 1 - Install cc_args.py
-;; 2 - bla bla
