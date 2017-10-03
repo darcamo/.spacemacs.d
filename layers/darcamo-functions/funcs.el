darcamo/.spacemacs.d

commit 13d5550d99fa21a15595d606353c07c245f4c7af

If nautilus is not installed, browse-dir will try to use pcmanfm

layers/darcamo-functions/funcs.el
@@ -177,19 +177,28 @@
 
 (defvar darlan/default-terminal-program "/usr/bin/gnome-terminal")
 
+(defun get-filemanager-fullpath ()
+  "Return path to nautilus, if installed, or to pcmanfm. If
+ neither is installed, return nil."
+  (setq filemanager (executable-find "nautilus"))
+  (if (equal filemanager nil)
+      (setq filemanager (executable-find "pcmanfm")))
+  )
+
 ;; Browse Directory
 (defun browse-dir (dir-as-string)
   (start-process-shell-command
    "browse"
    "*scratch*"
-   (concat "/usr/bin/nautilus --no-desktop " (concat "\"" (expand-file-name dir-as-string) "\""))))
+   ;; Trocar nautilus por pcmanfm para o lxde
+   (concat (get-filemanager-fullpath) " --no-desktop " (concat "\"" (expand-file-name dir-as-string) "\""))))
 
 ;; Use essa para abrir links sftp com o nautilus, como sftp://darlan@ssh.gtel.ufc.br/home/gtels0/projetos/ufc32
 (defun browse-dir-sftp (dir-as-string)
   (start-process-shell-command
    "browse"
    "*scratch*"
-   (concat "/usr/bin/nautilus --no-desktop " dir-as-string)))
+   (concat (get-filemanager-fullpath) " --no-desktop " dir-as-string)))
 
 (defun browse-default-dir ()
   "Open Nautilus in the default directory, which is the directory

commit 25a19d9f6d14a6f8340fb136d249fbbb68b7fb4d

Fix a bug in get-filemanager-fullpath function introduced in 13d5550

layers/darcamo-functions/funcs.el
@@ -183,6 +183,7 @@
   (setq filemanager (executable-find "nautilus"))
   (if (equal filemanager nil)
       (setq filemanager (executable-find "pcmanfm")))
+  filemanager
   )
 
 ;; Browse Directory