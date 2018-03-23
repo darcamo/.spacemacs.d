c02103e7c5ec1980a763e15429ec3857ef7368a0
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-03-23 11:28:23 -0300

    Make org mode use latexmk on latex export and add a new export class
    The new export class for latex export is "sbrt".

--- layers/darcamo-org-export/config.el
+++ layers/darcamo-org-export/config.el
@@ -15,3 +15,36 @@
           (indent "2em")
           (mathml nil)))
   )
+
+
+(with-eval-after-load 'ox-latex
+  (setq org-latex-pdf-process
+        ;; This requires latexmk, but it has many advantages such as running
+        ;; latex the correct number of times, support calling biber when
+        ;; biblatex is used, etc
+         '("latexmk -shell-escape -bibtex -pdf %f"))
+  )
+
+
+(with-eval-after-load 'ox-latex
+  (add-to-list
+   'org-latex-classes
+   '("sbrt"
+     "\\documentclass{sbrt2017eng}
+\\usepackage{hyperref}
+\\usepackage[nolist]{acronym}
+\\usepackage[backend=biber,doi=false,isbn=false, url=false, sorting=none, maxbibnames=3, giveninits=true, dateabbrev=true]{biblatex}
+\\addbibresource{~/Dropbox/org/references/articles.bib}
+\\usepackage{amssymb}
+\\usepackage{amsmath}
+\\usepackage{tikz}
+\\usepackage{MathDefinitions}
+[NO-DEFAULT-PACKAGES]  % do not include any of the default packages
+[PACKAGES]             % \\usepackage statements for packages
+[EXTRA]                % the stuff from #+LATEX_HEADER(_EXTRA)
+     "
+     ("\\section{%s}" . "\\section*{%s}")
+     ("\\subsection{%s}" . "\\subsection*{%s}")
+     ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+     ("\\paragraph{%s}" . "\\paragraph*{%s}")
+     ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
