674d53375fe3fd3a15de21a07ac4271051e3a16e
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-06-05 15:07:47 -0300

    Add keybindings for next/previous symbol in C++ layer

--- layers/darcamo-c++/keybindings.el
+++ layers/darcamo-c++/keybindings.el
@@ -6,6 +6,8 @@
   (local-set-key [C-f9] 'darlan-save-and-compile-single-file)  ;; Compile only the current source file
   (local-set-key [S-f9] 'darlan-generate-documentation)  ;; Generate the doxygen documentation
   (local-set-key [C-S-f9] 'darlan-open-doxygen-generated-index)
+  (local-set-key (kbd "M-n") 'highlight-symbol-next)
+  (local-set-key (kbd "M-p") 'highlight-symbol-prev)
   )
 
 (add-hook 'c-mode-common-hook 'darcamo-set-c-mode-common-keybindings)
