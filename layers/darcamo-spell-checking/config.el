a1df7711947cbf6b569cf10da31ae364b2e4b93a
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-06-06 14:45:09 -0300

    Use Hunspell for spell checking instead of Aspell
    The suggestions seem better

--- layers/darcamo-spell-checking/config.el
+++ layers/darcamo-spell-checking/config.el
@@ -1,7 +1,21 @@
+;; find aspell and hunspell automatically
+(cond
+ ;; try hunspell at first
+ ;; if hunspell does NOT exist, use aspell
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary-alist
+        ;; Please note the list `("-d" "en_US")` contains ACTUAL parameters passed to hunspell
+        ;; You could use `("-d" "en_US,en_US-med")` to check with multiple dictionaries
+        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)
+          )))
 
-;; Default Dictionary
-;; English as default for spell checking
-(set-default 'ispell-local-dictionary "english")
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  ;; (set-default 'ispell-local-dictionary "english")
+  ;; Please note ispell-extra-args contains ACTUAL parameters passed to aspell
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
 
 
 
