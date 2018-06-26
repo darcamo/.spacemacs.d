670ced73121b91265adeca8d997b22b58b8d5a3e
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-06-26 15:07:11 -0300

    Add mu4e-jump-to-list to mu4e layer

--- layers/darcamo-mu4e/packages.el
+++ layers/darcamo-mu4e/packages.el
@@ -30,7 +30,7 @@
 ;;; Code:
 
 (defconst darcamo-mu4e-packages
-  '()
+  '(mu4e-jump-to-list)
   "The list of Lisp packages required by the darcamo-mu4e layer.
 
 Each entry is either:
@@ -58,5 +58,9 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
+(defun darcamo-mu4e/init-mu4e-jump-to-list ()
+  (use-package mu4e-jump-to-list
+    :ensure t)
+  )
 
 ;;; packages.el ends here
