3b0ad931d24e354634952c895775de17d2de7421
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-10-19 15:18:31 -0300

    Fix darcamo-latex-extra layer
    Remove some code that does not work anymore

--- layers/darcamo-latex-extra/config.el
+++ layers/darcamo-latex-extra/config.el
@@ -3,13 +3,8 @@
   ;; Set auctex-mode for tex files.
   (add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
 
-  ;; (setq TeX-auto-save t)
-  ;; (setq TeX-parse-self t)
-
   ;; I often use \include or \input in my latex documents
-  (setq-default TeX-master nil)
-
-  ;; (setq reftex-plug-into-AUCTeX t)
+  ;; (setq-default TeX-master nil)
 
   ;; Here we have the default value of TeX-view-program-selection, but we only
   ;; changed output-pdf from "Evince" to "PDF Tools"
@@ -38,26 +33,16 @@
 
 (setq LaTeX-mode-hook
       (function (lambda ()
-                  ;; Add my local auctex style file folder to the path
-                  ;; (add-auctex-folder-to-path)
-
-                  ;; (load-library "beamer") ; If beamer is not found, add the
-                  ;;                         ; auctex styles folder to the
-                  ;;                         ; load path (TeX-add-style-hook
-                  ;;                         ; "beamer" 'my-beamer-mode)
-                  (turn-on-reftex)
-
                   (outline-minor-mode t)
                   (setq ispell-filter "tex")
 
                   ;; Minor-mode that highlights the symbol under
                   ;; point throughout the buffer
                   (highlight-symbol-mode)
-                  (darlan/set-highlight-symbol-key-bindings)
 
-                  ;; ;; Activates my beamer minor mode if the file name is "presentation.tex"
-                  ;; (if (string-equal (file-name-nondirectory buffer-file-name) "presentation.tex")
-                  ;;     (beamer-mode))
+                  ;; Activates my beamer minor mode if the file name is "presentation.tex"
+                  (if (string-equal (file-name-nondirectory buffer-file-name) "presentation.tex")
+                      (beamer-mode))
                   ))
       )
 
@@ -77,9 +62,6 @@
   ;; ;; Used for fold-mode in latex
   ;; (setq TeX-fold-macro-spec-list (quote (("[f]" ("footnote")) ("[c]" ("cite")) ("[l]" ("label")) ("[r]" ("ref" "pageref")) ("[i]" ("index")) ("*" ("item")) ("..." ("dots")) (1 ("part" "chapter" "section" "subsection" "subsubsection" "paragraph" "subparagraph" "part*" "chapter*" "section*" "subsection*" "subsubsection*" "paragraph*" "subparagraph*" "emph" "textit" "textsl" "textmd" "textrm" "textsf" "texttt" "textbf" "textsc" "textup" "ac" "acf" "acs" "acp" "acsp" "acfp" "acused")) (2 ("textcolor")))))
 
-  ;; (setq TeX-output-view-style (quote (("^dvi$" ("^landscape$" "^pstricks$\\|^pst-\\|^psfrag$") "%(o?)dvips -t landscape %d -o && gv %f") ("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "%(o?)dvips %d -o && ps2pdf %f && evince %s.pdf") ("^dvi$" ("^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "^landscape$") "%(o?)xdvi %dS -paper a4r -s 0 %d") ("^dvi$" "^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "%(o?)xdvi %dS -paper a4 %d") ("^dvi$" ("^\\(?:a5\\(?:comb\\|paper\\)\\)$" "^landscape$") "%(o?)xdvi %dS -paper a5r -s 0 %d") ("^dvi$" "^\\(?:a5\\(?:comb\\|paper\\)\\)$" "%(o?)xdvi %dS -paper a5 %d") ("^dvi$" "^b5paper$" "%(o?)xdvi %dS -paper b5 %d") ("^dvi$" "^letterpaper$" "%(o?)xdvi %dS -paper us %d") ("^dvi$" "^legalpaper$" "%(o?)xdvi %dS -paper legal %d") ("^dvi$" "^executivepaper$" "%(o?)xdvi %dS -paper 7.25x10.5in %d") ("^dvi$" "." "%(o?)xdvi %dS %d") ("^pdf$" "." "evince %s.pdf") ("^html?$" "." "netscape %o"))))
-
-
   ;; (setq font-latex-user-keyword-classes (quote (("textcolor{red}" ("textcolor{red}") (:foreground "red") (command 1)) ("textcolor{blue}" ("textcolor{blue}") (:foreground "blue") (command 1)) ("acronym" ("ac" "acf" "acs" "acl" "acp" "acsp" "aclp" "acfp" "acused") (:foreground "blue") (command 1)) ("acronym2" ("acro") nil (command 2)))))
 
   ;; ;; Set this to nil and Auctex won't fontify subscript and superscript
@@ -87,12 +69,6 @@
   ;; ;; fonts
   ;; (setq font-latex-fontify-script nil)
 
-  ;; ;; I want to use pdflatex by default
-  ;; (set-default 'TeX-PDF-mode t)
-
-  ;; (set-default 'TeX-source-correlate-mode t)
-
-  ;; (set-default 'TeX-fold-mode t)
   )
 
 
