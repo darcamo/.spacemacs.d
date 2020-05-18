e192ac128c6ac99637b886becccfe42e94b0978c
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-05-18 17:38:27 -0300

    Add some keybindings used in C++-mode to cmake-mode

--- layers/darcamo-c++/keybindings.el
+++ layers/darcamo-c++/keybindings.el
@@ -10,3 +10,13 @@
   ;; Enable key bindings (M-p and M-p) for navigation
   (highlight-symbol-nav-mode)
   )
+
+(with-eval-after-load 'cmake-mode
+  (define-key cmake-mode-map [M-f9] 'darlan-save-and-compile) ;; Ask for the target name and compile it
+  (define-key cmake-mode-map [f9] 'darlan-save-and-compile-last-target)  ;; Recompile the last target
+  (define-key cmake-mode-map [f10] 'darlan-run-last-target)
+  (define-key cmake-mode-map [M-f10] 'darlan-run-last-target-with-arguments)
+  (define-key cmake-mode-map [C-f9] 'darlan-save-and-compile-single-file)  ;; Compile only the current source file
+  (define-key cmake-mode-map [S-f9] 'darlan-generate-documentation)  ;; Generate the doxygen documentation
+  (define-key cmake-mode-map [C-S-f9] 'darlan-open-doxygen-generated-index)
+  )
