f6325a652ddd7618206383810c8389d90f4a68e1
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-06-19 19:41:47 -0300

    Add darcamo-org-roam layer

---
+++ layers/darcamo-org-roam/packages.el
@@ -0,0 +1,90 @@
+;;; packages.el --- darcamo-org-roam layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Darlan Cavalcante Moreira <darlan@darlan-desktop>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `darcamo-org-roam-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `darcamo-org-roam/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `darcamo-org-roam/pre-init-PACKAGE' and/or
+;;   `darcamo-org-roam/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst darcamo-org-roam-packages
+  '(org-roam)
+  "The list of Lisp packages required by the darcamo-org-roam layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+
+(defun darcamo-org-roam/init-org-roam ()
+  (use-package org-roam
+    :hook
+    (after-init . org-roam-mode)
+    :custom
+    (org-roam-directory "/home/darlan/org/org-roam-notes/")
+    :init
+    (progn
+      (spacemacs/declare-prefix "ar" "org-roam")
+      (spacemacs/set-leader-keys
+        "arl" 'org-roam
+        "art" 'org-roam-dailies-today
+        "arf" 'org-roam-find-file
+        "arg" 'org-roam-graph
+        "arj" 'org-roam-jump-to-index)
+
+      (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
+      (spacemacs/set-leader-keys-for-major-mode 'org-mode
+        "rl" 'org-roam
+        "rt" 'org-roam-dailies-today
+        "rb" 'org-roam-switch-to-buffer
+        "rf" 'org-roam-find-file
+        "ri" 'org-roam-insert
+        "rg" 'org-roam-graph
+        "rj" 'org-roam-jump-to-index)))
+  )
+
+
+;;; packages.el ends here
