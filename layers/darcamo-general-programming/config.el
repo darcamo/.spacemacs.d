9515e7d49c15c5bd7952e89d7823274bd128fbec
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-06-22 19:54:42 -0300

    Use prettify-symbols-mode programming modes

--- layers/darcamo-general-programming/config.el
+++ layers/darcamo-general-programming/config.el
@@ -1,1 +1,11 @@
 (set-default 'tab-width 4)
+
+(add-hook
+ 'prog-mode-hook
+ (lambda ()
+   (add-to-list 'prettify-symbols-alist '("<=" . "≤"))
+   (add-to-list 'prettify-symbols-alist '(">=" . "≥"))
+   (setq prettify-symbols-unprettify-at-point t)
+   (prettify-symbols-mode)
+   )
+ )
