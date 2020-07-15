fecb0c2e79dca6520e839417761dd4ec25292309
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-07-15 18:43:39 -0300

    Set f8 keybinding in org-mode buffers to org-toggle-link-display

--- layers/darcamo-org-general/keybindings.el
+++ layers/darcamo-org-general/keybindings.el
@@ -12,6 +12,7 @@
    (define-key org-mode-map [f6] 'darlan/org-surround-inline-math)
    (define-key org-mode-map [C-f6] 'org-insert-example-block)
    (define-key org-mode-map [C-M-return] 'org-meta-return)
+   (define-key org-mode-map [f8] 'org-toggle-link-display)
    ;; (define-key org-mode-map "\C-xye" 'yas/expand)
    ;; (define-key org-mode-map "\C-xyn" 'yas/next-field)
    ;; (define-key org-mode-map [tab] 'yas/expand)
