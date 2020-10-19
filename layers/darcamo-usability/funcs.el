9858569e3a0d6ac4c77caee3602ab6c348f04f31
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-10-19 19:47:40 -0300

    Remove defadvice of kill-ring-save
    The advice copied a line when the selection was not active.
    However, currently selection is not working in dired-mode and with the defadvice
    it was impossible to copy a region.
    
    The defadvice should be included if the mark works in dired again

--- layers/darcamo-usability/funcs.el
+++ layers/darcamo-usability/funcs.el
@@ -2,14 +2,14 @@
 
 
 ;; xxxxxxxxxx Muda o comportamento de C-w e M-w xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-(defadvice kill-ring-save (before slick-copy activate compile)
-  "When called interactively with no active region, copy a single line instead."
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (message "Copied line")
-     (list (line-beginning-position) (line-beginning-position
-                                      2))
-     )))
+;; (defadvice kill-ring-save (before slick-copy activate compile)
+;;   "When called interactively with no active region, copy a single line instead."
+;;   (interactive
+;;    (if mark-active (list (region-beginning) (region-end))
+;;      (message "Copied line")
+;;      (list (line-beginning-position) (line-beginning-position
+;;                                       2))
+;;      )))
 
 ;; spacemacs has a different keybinding for C-w, but there is a command to kill
 ;; the whole line with C-S-<backspace>
