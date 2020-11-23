06ec1d6b76f0c1c8b6a75c98da81e15172230805
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-11-23 17:33:35 -0300

    Add f9 keybinding in org-mode buffers to org-export-dispatch
    Add f9 keybinding in org-mode buffers to org-export-dispatch with universal
    argument, which means "export again to the last exported target".

--- layers/darcamo-org-general/keybindings.el
+++ layers/darcamo-org-general/keybindings.el
@@ -11,6 +11,8 @@
  (lambda ()
    (define-key org-mode-map [C-M-return] 'org-meta-return)
    (define-key org-mode-map [f8] 'org-toggle-link-display)
+   (define-key org-mode-map [f9] '(lambda () (interactive) (if org-export-dispatch-last-action (org-export-dispatch 4) (message "Please export to some target first")) ))
+
    ;; This will open a PDF file with the same name of the org-mode file
    ;; Useful when you are exporting the org-mode buffer to PDF
    (define-key org-mode-map [f10] '(lambda () (interactive) (find-file (concat (file-name-sans-extension (file-name-nondirectory (buffer-file-name))) ".pdf"))))
