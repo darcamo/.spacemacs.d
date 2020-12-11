0398d637f03cd0aae97ec7fa2028badcdd0e94b0
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-12-11 11:26:34 -0300

    Add S-f8 keybinding in org-mode to org-toggle-inline-images
    This plays nice with my f8 keybinding to org-toggle-link-display

--- layers/darcamo-org-general/keybindings.el
+++ layers/darcamo-org-general/keybindings.el
@@ -11,6 +11,7 @@
  (lambda ()
    (define-key org-mode-map [C-M-return] 'org-meta-return)
    (define-key org-mode-map [f8] 'org-toggle-link-display)
+   (define-key org-mode-map [S-f8] 'org-toggle-inline-images)
 
    ;; This will open a PDF file with the same name of the org-mode file
    ;; Useful when you are exporting the org-mode buffer to PDF
