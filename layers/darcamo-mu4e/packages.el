2445e5cd340b01009d62d71d288299cab45c3293
Darlan Cavalcante Moreira <darcamo@gmail.com>
2021-04-19 13:46:42 -0300

    Add mu4e-views in darcamo-mu4e

--- layers/darcamo-mu4e/packages.el
+++ layers/darcamo-mu4e/packages.el
@@ -31,7 +31,9 @@
 
 (defconst darcamo-mu4e-packages
   '(mu4e-jump-to-list
-    org-msg)
+    org-msg
+    mu4e-views
+    )
   "The list of Lisp packages required by the darcamo-mu4e layer.
 
 Each entry is either:
@@ -94,6 +96,26 @@ Each entry is either:
  -- *Darlan* \\\\
  #+end_signature")
     (org-msg-mode)
+    )
+  )
+
+(defun darcamo-mu4e/init-mu4e-views ()
+  (use-package mu4e-views
+    :ensure t
+    :after mu4e
+    :defer nil
+    :bind (:map mu4e-headers-mode-map
+	            ("v" . mu4e-views-mu4e-select-view-msg-method) ;; select viewing method
+	            ("M-n" . mu4e-views-cursor-msg-view-window-down) ;; from headers window scroll the email view
+	            ("M-p" . mu4e-views-cursor-msg-view-window-up) ;; from headers window scroll the email view
+                ("f" . mu4e-views-toggle-auto-view-selected-message) ;; toggle opening messages automatically when moving in the headers view
+	            )
+    :config
+    (setq mu4e-views-completion-method 'ivy) ;; use ivy for completion
+    (setq mu4e-views-default-view-method "html") ;; make xwidgets default
+    (mu4e-views-mu4e-use-view-msg-method "html") ;; select the default
+    (setq mu4e-views-next-previous-message-behaviour 'stick-to-current-window) ;; when pressing n and p stay in the current window
+    (setq mu4e-views-auto-view-selected-message t)
     )
   )
 
